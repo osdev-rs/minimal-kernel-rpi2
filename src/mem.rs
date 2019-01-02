d217a2549258be5cdbb4e666e2d0226b96b50d9b
papamitra <papamitra@gmail.com>
2019-01-02 23:14:42 +0900

    Remove spin::Mutex

--- src/mem.rs
+++ src/mem.rs
@@ -1,5 +1,4 @@
 use core::alloc::{GlobalAlloc, Layout};
-use spin::Mutex;
 use core::mem::size_of;
 
 #[derive(Clone,Copy)]
@@ -15,46 +14,37 @@ extern {
     static __kernel_heap_end__: *mut u8;
 }
 
-static mut FREES: Mutex<usize> = Mutex::new(0usize);
-static mut FREE: Mutex<[FreeInfo; 4090]> = Mutex::new([FreeInfo{addr:0,size:0}; 4090]);
+static mut FREES: usize = 0;
+static mut FREE: [FreeInfo; 4090] = [FreeInfo{addr:0,size:0}; 4090];
 
 use super::uart;
 
 pub unsafe fn init() {
-    let mut fs = FREES.lock();
-    let mut free = FREE.lock();
 
-    *fs = 1;
-    free[0] = FreeInfo{addr: 0x2000000,
-//addr: unsafe {__kernel_heap_start__ as u32},
-//                       size: unsafe {__kernel_heap_end__.wrapping_offset_from(
-//                           __kernel_heap_start__) as usize } * size_of::<u32>()};
+    FREES = 1;
+    FREE[0] = FreeInfo{addr: 0x2000000,
                        size: 0x100000};
 
-//    if free[0].size <= 0 {
-//        uart::write("free[0].size <= 0\n");
-//    }
-
-//    uart::write("mem::init()\n");
+//    FREE[0] = FreeInfo{
+//        addr: unsafe {__kernel_heap_start__ as u32},
+//        size: unsafe {__kernel_heap_end__.wrapping_offset_from(
+//            __kernel_heap_start__) as usize } * size_of::<u32>()};
 
 }
 
 unsafe impl GlobalAlloc for KernelAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let mut fs = FREES.lock();
-        let mut free = FREE.lock();
-
-        for i in 0..*fs {
+        for i in 0..FREES {
             let size = layout.align();
 
-            if free[i].size >= size {
-                let addr = free[i].addr as *mut u8;
-                free[i].addr += size as u32;
-                free[i].size -= size;
-                if free[i].size == 0 {
-                    *fs -= 1;
-                    for j in i..*fs {
-                        free[j] = free[j+1];
+            if FREE[i].size >= size {
+                let addr = FREE[i].addr as *mut u8;
+                FREE[i].addr += size as u32;
+                FREE[i].size -= size;
+                if FREE[i].size == 0 {
+                    FREES -= 1;
+                    for j in i..FREES {
+                        FREE[j] = FREE[j+1];
                     }
                 }
                 return addr
